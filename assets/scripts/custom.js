b520197bd795138309427bfe5aa326140f43f2c3
Filippo Pesavento <pesaventofilippo@gmail.com>
2019-03-04 13:53:40 +0100

    Added Theme Switch (dark mode enabled)

---
+++ assets/scripts/custom.js
@@ -0,0 +1,27 @@
+function switchTheme() {
+    var currentTheme = localStorage.currentTheme;
+    if(currentTheme == "default") {
+        localStorage.currentTheme = "dark";
+    }
+    else if(currentTheme == "dark") {
+        localStorage.currentTheme = "default";
+    }
+    location.reload();
+};
+
+
+window.onload = function() {
+    var currentTheme = localStorage.currentTheme;
+    if(!currentTheme) {
+        localStorage.currentTheme = "default";
+    }
+    var link_tag = document.getElementsByTagName("link");
+        for (var i=0; i<link_tag.length; i++) {
+            if ((link_tag[i].rel.indexOf("stylesheet") != -1) && link_tag[i].title) {
+                link_tag[i].disabled = true;
+                if (link_tag[i].title == currentTheme) {
+                    link_tag[i].disabled = false;
+                }
+            }
+        }
+};
